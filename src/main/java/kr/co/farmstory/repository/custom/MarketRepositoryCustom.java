818f24cf5fcc3b73884f5d8ac662c075bb8cb4de
skdlawo5985 <skdlawo5985@gmail.com>
2024-04-01 17:19:12 +0900

    17:20

---
+++ src/main/java/kr/co/farmstory/repository/custom/MarketRepositoryCustom.java
@@ -0,0 +1,13 @@
+package kr.co.farmstory.repository.custom;
+
+import kr.co.farmstory.dto.MarketPageRequestDTO;
+import kr.co.farmstory.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+public interface MarketRepositoryCustom {
+    // market/list 페이지 product 조회
+    public Page<Product> selectProducts(MarketPageRequestDTO marketPageRequestDTO, Pageable pageable);
+
+}
