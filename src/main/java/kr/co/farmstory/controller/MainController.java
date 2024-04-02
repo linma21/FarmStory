746712cd74dc5ac9a7fe081bb420a6be07a1ac34
qksmr5546 <qksrm5546@gmail.com>
2024-04-02 18:46:08 +0900

    index, SNAPSHOT modifed

--- src/main/java/kr/co/farmstory/controller/MainController.java
+++ src/main/java/kr/co/farmstory/controller/MainController.java
@@ -2,16 +2,29 @@ package kr.co.farmstory.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class MainController {
-    @GetMapping(value = {"/", "/index"})
-    public String index(){
 
+    private final BuildProperties buildProperties;
+
+
+    @GetMapping(value = {"/", "/index"})
+    public String index(Model model){
+
+
+
+        // 상단 BuildProperties 주입
+        String appVersion = buildProperties.getVersion();
+        log.info(appVersion);
+
+        model.addAttribute("appVersion",appVersion);
         return "/index";
     }
 
