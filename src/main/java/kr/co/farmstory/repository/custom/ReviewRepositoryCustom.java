e4542fca1abcf18270634cef58f69569e4f919e4
chlvldzmf <chlvldzmf@gmail.com>
2024-04-09 21:36:58 +0900

    review Ratio

--- src/main/java/kr/co/farmstory/repository/custom/ReviewRepositoryCustom.java
+++ src/main/java/kr/co/farmstory/repository/custom/ReviewRepositoryCustom.java
@@ -3,6 +3,7 @@ package kr.co.farmstory.repository.custom;
 
 import com.querydsl.core.Tuple;
 import kr.co.farmstory.dto.ReviewPageRequestDTO;
+import kr.co.farmstory.dto.ReviewRatioDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -11,4 +12,8 @@ import java.util.List;
 public interface ReviewRepositoryCustom {
 
     public Page<Tuple> selectReviewsAndNick(int prodno, ReviewPageRequestDTO pageRequestDTO, Pageable pageable);
+
+    public Tuple selectForRatio(int prodno);
+
+    public List<Tuple> selectScoreCount(int prodno);
 }
