cf5ee29bb720107b37c9749da0263a034e86f776
NAIMJAE <skdlawo5985@gamil.com>
2024-04-10 23:57:34 +0900

    배송날짜구현 / 상품등록썸네일처리

--- src/main/java/kr/co/farmstory/config/WebMvcConfig.java
+++ src/main/java/kr/co/farmstory/config/WebMvcConfig.java
@@ -35,7 +35,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     // imjae
     // private String resourcePath = "file:///Users/java/Desktop/workspace/farmstory/prodImg/";
-    //private String resourcePath = "file:///Users/devimjae/Desktop/workspace/farmstory/prodImg/";
+    private String resourcePath = "file:///Users/devimjae/Desktop/workspace/farmstory/prodImg/";
 
     // 배포
     // private String resourcePath = "file:///home/farmStory/prodImg/";
