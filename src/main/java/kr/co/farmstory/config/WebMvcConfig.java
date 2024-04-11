62fd00bef2af6b7d274a25021ddabeb337e44e34
NAIMJAE <153374452+NAIMJAE@users.noreply.github.com>
2024-04-11 09:07:24 +0900

    aa

--- src/main/java/kr/co/farmstory/config/WebMvcConfig.java
+++ src/main/java/kr/co/farmstory/config/WebMvcConfig.java
@@ -35,13 +35,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     //private String resourcePath = "file:///Users/java/Desktop/Workspace/farmstory/prodImg/";
 
     // 이진
-    private String resourcePath = "file:///Users/java/Desktop/workspace/farmstory/prodImg/";
+    //private String resourcePath = "file:///Users/java/Desktop/workspace/farmstory/prodImg/";
 
     // 태영
-    private String resourcePath = "file:///Users/java/Desktop/Workspace/farmstory/prodImg/";
+    //private String resourcePath = "file:///Users/java/Desktop/Workspace/farmstory/prodImg/";
 
     // imjae
-    // private String resourcePath = "file:///Users/java/Desktop/workspace/farmstory/prodImg/";
+    private String resourcePath = "file:///Users/java/Desktop/workspace/farmstory/prodImg/";
     //private String resourcePath = "file:///Users/devimjae/Desktop/workspace/farmstory/prodImg/";
 
     //private String resourcePath = "file:///Users/java/Desktop/workspace/farmstory/prodImg/";
@@ -50,9 +50,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     // 배포
     //private String resourcePath = "file:///home/farmStory/prodImg/";
-
-    @Value("${file.prodImg.path}")
-    private String resourcePath;
 
 
     @Override
